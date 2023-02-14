9a6bb609b5958d3c06d39b0eaa113647d8b7cd77
emoney <eric.mcmullen@gmail.com>
2023-02-14 11:11:38 -0300

    .

---
+++ src/canvas/MC.Canvas.Core/Rectangle.cs
@@ -0,0 +1,37 @@
+﻿namespace MC.Canvas.Core;
+
+using System;
+
+public readonly record struct Rectangle
+{
+    public Point2D Origin { get; init; }
+    public float Width { get; init; }
+    public float Height { get; init; }
+    public float Rotation { get; init; }
+
+    Rectangle(Point2D origin, float width, float height, float rotation)
+    {
+        if (width <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(width));
+        }
+
+        if (height <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(height));
+        }
+
+        this.Origin = origin;
+        this.Width = width;
+        this.Height = height;
+        this.Rotation = rotation % 360;
+    }
+
+    public Rectangle Resize(Rectangle original, float factor) =>
+        original with
+        {
+            Width = original.Width * factor,
+            Height = original.Height * factor,
+        };
+
+};
---
+++ src/canvas/MC.Canvas.Core/UFloat.cs
@@ -0,0 +1,42 @@
+﻿namespace MC.Canvas.Core;
+
+using System;
+
+/// <summary>
+/// 
+/// </summary>
+/// <remarks>
+/// Inspired by: https://stackoverflow.com/questions/7305785/does-c-sharp-have-an-unsigned-double
+/// </remarks>
+public readonly record struct UFloat
+{
+    public readonly float Value;
+
+    public UFloat(float value)
+    {
+        if (value < 0)
+        {
+            throw new ArgumentOutOfRangeException("value");
+        }
+        Value = value;
+    }
+
+    public static implicit operator float(UFloat uf)
+        => uf.Value;
+
+    public static implicit operator UFloat(float f)
+        => new(f);
+
+    public static bool operator <(UFloat a, UFloat b)
+        => a.Value < b.Value;
+
+    public static bool operator >(UFloat a, UFloat b)
+        => a.Value > b.Value;
+
+    public static bool operator <=(UFloat a, UFloat b)
+        => a.Value <= b.Value;
+
+    public static bool operator >=(UFloat a, UFloat b)
+        => a.Value >= b.Value;
+
+};
