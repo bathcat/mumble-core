9f074bc9a02aa16ae3d43b505563ea7b79afb11d
emoney <eric.mcmullen@gmail.com>
2023-02-15 09:47:11 -0300

    .

---
+++ src/fizzbuzz.console/startingpoint/MC.FizzBuzz.Console.Tests/ProgramTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MC.FizzBuzz.Console.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void MainTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ src/fizzbuzz.console/startingpoint/MC.FizzBuzz.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace MC.FizzBuzz.Console;
+
+using System;
+
+public class Program
+{
+    public static int Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        return 0;
+    }
+}
