17412154cc9f6209fc6495db11013a85730709ac
emoney <eric.mcmullen@gmail.com>
2023-02-15 09:54:28 -0300

    Tidying.

---
+++ src/snakes/complete/MC.Snakes.Core/Snake.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MC.Snakes.Core;
+
+public class Snake
+{
+    public Guid ID { get; set; }
+    public string Name { get; set; }
+    public int MeannessLevel { get; set; }
+}
