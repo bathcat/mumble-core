bd871d60ebaa58e35ed423d805305df8d7a63e62
emoney <eric.mcmullen@gmail.com>
2023-02-23 10:15:10 -0300

    Add startingpoint.

---
+++ src/fizzbuzz.microservice/startingpoint/MC.FizzBuzz.Client/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace MC.FizzBuzz.Client;
+
+internal class Program
+{
+
+    private const string url = "http://localhost:5013";
+    static async Task<int> AsyncMain(string[] args)
+    {
+        using var client = new HttpClient();
+        using var response = await client.GetAsync(url);
+        var message = await response.Content.ReadAsStringAsync();
+        Console.WriteLine(message);
+        Console.ReadKey();
+        return 0;
+    }
+
+    public static int Main(string[] args)
+        => AsyncMain(args).Result;
+
+}
